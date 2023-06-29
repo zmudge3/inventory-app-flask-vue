bc8629cfc3ab4ec2b4f9e59174d3a128ea26a2ed
Zack Mudge <zmudge@gatech.edu>
2023-06-29 16:42:27 -0400

    Change dummy data to dictionaries

--- server/app.py
+++ server/app.py
@@ -9,58 +9,47 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-ITEMS = [
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item1-1',
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item1-2',
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item2-1',
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item2-2',
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item3-1',
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Item3-2',
-    },
-]
-
-CONTAINERS = [
-    {
-        'id': uuid.uuid4().hex,
+CONTAINERS = {
+    'd9cb84d4c2384b65aaa31f8be7aedfa8': {
         'name': 'Plastic Tote 1',
-        'items': [ITEMS[0]['id'], ITEMS[1]['id']],
+        'items': ['19de7cac23f6483ca82552b3d9c98205', '3d4c0fab1eb841e8bbef6e67f148fd77'],
     },
-    {
-        'id': uuid.uuid4().hex,
+    '04bab8f1da1d40a88fa02c5620478608': {
         'name': 'Plastic Tote 2',
-        'items': [ITEMS[2]['id'], ITEMS[3]['id']],
+        'items': ['5f2ecff9037d4fc88b8539d9f5b1e9e1', 'e5a337699083470d9c007529531f9fd5'],
     },
-    {
-        'id': uuid.uuid4().hex,
+    '4107de7cf55646e48b823b36b0841eff': {
         'name': 'Drawer 1',
-        'items': [ITEMS[4]['id'], ITEMS[5]['id']],
+        'items': ['60290617555f4a548d60c55e0eaae4f3', 'b02c9bc1e124408284708df3d9ba396b'],
     },
-]
+}
 
-ITEMS[0]['container_id'] = CONTAINERS[0]['id']
-ITEMS[1]['container_id'] = CONTAINERS[0]['id']
-ITEMS[2]['container_id'] = CONTAINERS[1]['id']
-ITEMS[3]['container_id'] = CONTAINERS[1]['id']
-ITEMS[4]['container_id'] = CONTAINERS[2]['id']
-ITEMS[5]['container_id'] = CONTAINERS[2]['id']
-
+ITEMS = {
+    '19de7cac23f6483ca82552b3d9c98205': {
+        'name': 'Item1-1',
+        'container_id': 'd9cb84d4c2384b65aaa31f8be7aedfa8',
+    },
+    '3d4c0fab1eb841e8bbef6e67f148fd77': {
+        'name': 'Item1-2',
+        'container_id': 'd9cb84d4c2384b65aaa31f8be7aedfa8',
+    },
+    '5f2ecff9037d4fc88b8539d9f5b1e9e1': {
+        'name': 'Item2-1',
+        'container_id': '04bab8f1da1d40a88fa02c5620478608',
+    },
+    'e5a337699083470d9c007529531f9fd5': {
+        'name': 'Item2-2',
+        'container_id': '04bab8f1da1d40a88fa02c5620478608',
+    },
+    '60290617555f4a548d60c55e0eaae4f3': {
+        'name': 'Item3-1',
+        'container_id': '4107de7cf55646e48b823b36b0841eff',
+    },
+    'b02c9bc1e124408284708df3d9ba396b': {
+        'name': 'Item3-2',
+        'container_id': '4107de7cf55646e48b823b36b0841eff',
+    },
+}
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
@@ -78,11 +67,11 @@ def all_containers():
 def new_container():
     response_object = {'status': 'success'}
     post_data = request.get_json()
-    CONTAINERS.append({
-        'id': uuid.uuid4().hex,
+    new_container_id = uuid.uuid4().hex
+    CONTAINERS[new_container_id] = {
         'name': post_data.get('name'),
         'items': []
-    })
+    }
     response_object['message'] = 'Container added'
     return jsonify(response_object)
 
