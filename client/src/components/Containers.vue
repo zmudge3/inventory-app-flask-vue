eb70d974f950435b93780b7a887afdfd8dd38c1f
Zack Mudge <zmudge@gatech.edu>
2023-07-08 15:39:04 -0400

    Edit/delete container

--- client/src/components/Containers.vue
+++ client/src/components/Containers.vue
@@ -6,6 +6,14 @@
       @showContainerList="showContainerList = $event">
     </containerNewForm>
   </div>
+  <div class="container">
+    <containerEditForm
+      :containerForEdit="containerForEdit"
+      v-if="showContainerEditForm"
+      @showContainerEditForm="showContainerEditForm = $event"
+      @showContainerList="showContainerList = $event">
+    </containerEditForm>
+  </div>
   <div class="container" v-if="showContainerList && loaded">
     <div class="row">
       <div class="col-sm-10">
@@ -24,6 +32,7 @@
               <th scope="col">Name</th>
               <th scope="col"># Items</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -37,7 +46,23 @@
                     class="btn btn-primary btn-sm"
                     @click="handleViewButton(container.id)">
                     View Items
-                </button>
+                  </button>
+                </div>
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button
+                    type="button"
+                    class="btn btn-warning btn-sm"
+                    @click="handleEditButton(container)">
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-danger btn-sm"
+                    @click="handleDeleteButton(container.id)">
+                    Delete
+                  </button>
                 </div>
               </td>
             </tr>
@@ -52,12 +77,15 @@
 import axios from 'axios';
 import Alert from './Alert.vue';
 import ContainerNewForm from './ContainerNewForm.vue';
+import ContainerEditForm from './ContainerEditForm.vue';
 
 export default {
   data() {
     return {
       containers: [],
       showContainerNewForm: false,
+      showContainerEditForm: false,
+      containerForEdit: null,
       showContainerList: true,
       message: '',
       showMessage: false,
@@ -67,6 +95,7 @@ export default {
   components: {
     alert: Alert,
     containerNewForm: ContainerNewForm,
+    containerEditForm: ContainerEditForm,
   },
   methods: {
     getContainers() {
@@ -93,6 +122,27 @@ export default {
     },
     handleViewButton(containerID) {
       this.$router.push({name: 'ContainerItems', params: {containerID: containerID}});
+    },
+    handleDeleteButton(containerID) {
+      this.removeContainer(containerID);
+    },
+    removeContainer(containerID) {
+      const path = `http://localhost:5001/containers/${containerID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getContainers();
+          this.handleMessage('Container deleted');
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getContainers();
+        });
+    },
+    handleEditButton(container) {
+      this.showContainerEditForm = true;
+      this.showContainerList = false;
+      this.showContainerNewForm = false;
+      this.containerForEdit = container;
     },
   },
   created() {
